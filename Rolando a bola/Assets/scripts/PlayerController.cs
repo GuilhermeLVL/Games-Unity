e00de43b6dae35a18f0b5d59342ef82f701745c4
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-11 20:12:30 -0300

    Update PlayerController.cs

--- Rolando a bola/Assets/scripts/PlayerController.cs
+++ Rolando a bola/Assets/scripts/PlayerController.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //Criacao de variavel.
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();   
     }
 
     private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX,0.0f ,movementY);
+        rb.AddForce(movement);
     }
 
+    //Funcao sem retorno, apenas executa uma tarefa
+     void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 }
