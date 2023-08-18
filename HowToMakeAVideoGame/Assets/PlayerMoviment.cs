2cc80af5ebbf8a2893d94e3add33d73bb141bdb2
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-18 16:38:08 -0300

    direita e esquerda

--- HowToMakeAVideoGame/Assets/PlayerMoviment.cs
+++ HowToMakeAVideoGame/Assets/PlayerMoviment.cs
@@ -5,17 +5,23 @@ public class PlayerMoviment : MonoBehaviour
 {
 
     public Rigidbody rb;
-    
 
+    public float forwardForce = 2000f;
 
     
 
     // Update is called once per frame
 
-    //Time.delta, faz com que a taxa de atualizacao seja adequada
+    //  {[* Time.delta *]}, faz com que a taxa de atualizacao seja adequada
 
     void Update()
     {
-        rb.AddForce (0, 0, 2000 * Time.deltaTime);
+        rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 *  Time.deltaTime, 0, 0);
+        }
+
     }
 }
