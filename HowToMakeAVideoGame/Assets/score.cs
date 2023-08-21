9706356a4b4eb601ffee0fe785467dd34291c90c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-21 20:56:21 -0300

    Update score.cs

--- HowToMakeAVideoGame/Assets/score.cs
+++ HowToMakeAVideoGame/Assets/score.cs
@@ -1,14 +1,14 @@
 
 using UnityEngine;
-
+using UnityEngine.UI;
 public class score : MonoBehaviour
 {
     public Transform player;
-  
+    public Text scoreText;
    
 
     void Update()
     {
-       Debug.Log( player.position.z);
+        scoreText.text = player.position.z.ToString();
     }
 }
