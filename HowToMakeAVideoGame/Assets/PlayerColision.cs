b8b1554de4f1baa167ac77cba328a3b8e37f5896
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-21 15:23:25 -0300

    Fisica

---
+++ HowToMakeAVideoGame/Assets/PlayerColision.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("vOCE BATEU EM ALGO");
+    }
+}
