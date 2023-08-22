59a4e551553759ca8b8c1eca50cb720740e351a6
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-22 20:09:10 -0300

    .

--- HowToMakeAVideoGame/Assets/GameManager.cs
+++ HowToMakeAVideoGame/Assets/GameManager.cs
@@ -1,12 +1,26 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-    
+    bool gameHasEnded = false;
+
+    public float restatDelay = 1f;
     public void EndGame ()
     {
-   
+   if (gameHasEnded  == false)
+        {
+
+            gameHasEnded = true;
+            Invoke("Restart", restatDelay);
+        }
+        
+    }
+
+    void Restart() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
