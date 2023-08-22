048276acab983ca021ad4d57937b63a795598121
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-22 19:45:02 -0300

    .

---
+++ HowToMakeAVideoGame/Assets/GameManager.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+    public void EndGame ()
+    {
+   
+    }
+}
--- HowToMakeAVideoGame/Assets/Scripts/PlayerColision.cs
+++ HowToMakeAVideoGame/Assets/Scripts/PlayerColision.cs
@@ -1,16 +1,18 @@
 
 using UnityEngine;
 
+
 public class PlayerColision : MonoBehaviour
 {
     public PlayerMoviment moviment;
-
+    
 
     private void OnCollisionEnter(Collision collisionInfo)
     {
       if (collisionInfo.collider.tag == "Obstaculo") 
         {
             moviment.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
