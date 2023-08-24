c01f6ccc39a4ec7dc9df31de3f4b5645189cda2f
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-24 15:29:34 -0300

    s

--- HowToMakeAVideoGame/Assets/LevelComplete.cs
+++ HowToMakeAVideoGame/Assets/LevelComplete.cs
@@ -1,11 +1,12 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
 
     public void LoadNextLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)
     }
 }
