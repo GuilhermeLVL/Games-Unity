0ffd69bb361795e2d5f2583c6d47412649e2e513
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-24 16:44:15 -0300

    a

--- HowToMakeAVideoGame/Assets/Scripts/LevelComplete.cs
+++ HowToMakeAVideoGame/Assets/Scripts/LevelComplete.cs
@@ -7,6 +7,6 @@ public class LevelComplete : MonoBehaviour
 
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
