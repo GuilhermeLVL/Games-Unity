061ab1348a401fd9fa83ceb8c5cb39b5ca2dd904
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-25 20:20:54 -0300

    Update Player.cs

--- EndlessRunnerSeries-master/Assets/Scripts/Player.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/Player.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Player : MonoBehaviour { 
         
-    }
+    
+    public float Yincrement;
 
-    // Update is called once per frame
+    private Vector2 targetPos;
+
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+    }
     }
 }
