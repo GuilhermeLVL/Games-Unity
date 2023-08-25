0839c8815f31e59ca1807cdd55007c3953b287f2
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-25 20:42:32 -0300

    Update Player.cs

--- EndlessRunnerSeries-master/Assets/Scripts/Player.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/Player.cs
@@ -12,21 +12,28 @@ public class Player : MonoBehaviour {
 
     public float speed;
 
+    public float maxHeight;
+    public float minHeight;
+
 
     void Update()
     {
-
+        // Move o player para o centro da tela com a velocidade da variavel speed
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        //Funcao que pega as teclas digitadas e (cima e baixo)
+        // para deslocar o player no eixo Y
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
             transform.position = targetPos;
 
-        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
     {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
             transform.position = targetPos;
         }
+
+
     }
 }
