8e58a908a1bf9829b171c5e6906786b3bb9df161
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-25 20:27:22 -0300

    Update Player.cs

--- EndlessRunnerSeries-master/Assets/Scripts/Player.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/Player.cs
@@ -10,9 +10,14 @@ public class Player : MonoBehaviour {
 
     private Vector2 targetPos;
 
+    public float speed;
+
 
     void Update()
     {
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
