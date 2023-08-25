1719e1bf7a685cad94a37a735b12fbc5421232a1
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-25 20:24:12 -0300

    Update Player.cs

--- EndlessRunnerSeries-master/Assets/Scripts/Player.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/Player.cs
@@ -16,10 +16,12 @@ public class Player : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
 
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
     {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-    }
+            transform.position = targetPos;
+        }
     }
 }
