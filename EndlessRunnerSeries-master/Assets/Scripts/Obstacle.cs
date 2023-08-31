28edc546e0f446fba24aa44ca0cea14c23d8fbf6
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-31 19:00:25 -0300

    Update Obstacle.cs

--- EndlessRunnerSeries-master/Assets/Scripts/Obstacle.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/Obstacle.cs
@@ -6,6 +6,9 @@ public class Obstacle : MonoBehaviour
 {
     public int damage = 1;
     public float speed;
+
+    public GameObject effect;
+
     
 
     private void Update()
@@ -18,6 +21,8 @@ public class Obstacle : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+
+            Instantiate(effect, transform.position, Quaternion.identity);
             other.GetComponent<Player>().health -= damage;
 
             Destroy(gameObject);
