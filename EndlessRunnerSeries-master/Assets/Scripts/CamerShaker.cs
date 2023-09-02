69500ec2b27d2c40a14b0e7146b8af5852561d66
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-01 23:21:44 -0300

    camera shake

---
+++ EndlessRunnerSeries-master/Assets/Scripts/CamerShaker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerShaker : MonoBehaviour
+{
+    [Header("Camera Shaker Config")]
+    Vector3 cameraInitialPosition;
+    public float shakeMagnitude = 0.05f;
+    public float shakeTime = 0.5f;
+    public Camera mainCamera;
+
+    
+    public void ShakeIt()
+    {
+
+
+    }
+
+    void StartCameraShaking()
+    {
+
+    }
+
+    
+}
