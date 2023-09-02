8b748c0ae25d3e83c9ce9e8d86782f7207183e20
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-02 01:25:58 -0300

    .

--- EndlessRunnerSeries-master/Assets/Scripts/SCOREManage.cs
+++ EndlessRunnerSeries-master/Assets/Scripts/SCOREManage.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+using System.Xml.Serialization;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     
     public int score;
+
+    public TextMeshProUGUI scoreDisplay;
+
+    private void Update()
+    {
+        scoreDisplay.text = score.ToString();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     { 
 
