dfb00ac5177d870d476db57cd58e62a56f5c8561
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-05 20:26:03 -0300

    Update Player.cs

--- Fredi-Primeiro_Jogo/Assets/Scripts/Player.cs
+++ Fredi-Primeiro_Jogo/Assets/Scripts/Player.cs
@@ -7,20 +7,21 @@ public class Player : MonoBehaviour
     public float forceMultiplier = 3f;
     public float maximumVelocity = 3f;
 
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         var HorizontalInput = Input.GetAxis("Horizontal");
-        if (GetComponent<Rigidbody>().velocity.magnitude <= maximumVelocity )
+        if (rb.velocity.magnitude <= maximumVelocity )
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(HorizontalInput * forceMultiplier,0,0));
+            rb.AddForce(new Vector3(HorizontalInput * forceMultiplier,0,0));
 
         }
 
