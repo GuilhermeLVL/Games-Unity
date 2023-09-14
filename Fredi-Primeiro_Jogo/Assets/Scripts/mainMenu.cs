ba8a067bb4e06414546ee363610b2565fa4294b0
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-13 22:59:48 -0300

    .

--- Fredi-Primeiro_Jogo/Assets/Scripts/mainMenu.cs
+++ Fredi-Primeiro_Jogo/Assets/Scripts/mainMenu.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
 
+
     public GameManager gameManager;
+
+    private void Start()
+    {
+        GetComponentInChildren<TMPro.TextMeshProUGUI>().gameObject.LeanScale(new Vector3(1.2f, 1.2f), 0.3f).setLoopPingPong();
+    }
     public void Play()
+    {
+        GetComponent<CanvasGroup>().LeanAlpha(0, 0.2f).setOnComplete(onComplete);
+      
+    }
+
+    private void onComplete()
     {
         gameManager.Enabled();
         Destroy(gameObject);
     }
-
 }
