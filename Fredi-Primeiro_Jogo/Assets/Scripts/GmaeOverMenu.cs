55289dfb5e394603f44bddf6e62d197540e74ca9
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-16 02:14:43 -0300

    1

--- Fredi-Primeiro_Jogo/Assets/Scripts/GmaeOverMenu.cs
+++ Fredi-Primeiro_Jogo/Assets/Scripts/GmaeOverMenu.cs
@@ -5,16 +5,7 @@ using UnityEngine;
 public class GmaeOverMenu : MonoBehaviour
 {
 
-    private void OnEnable()
-    {
-        var rectTransform = GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector2(0, rectTransform.rect.height);
-
-    }
-
-
-
-
+  
     public void Restart()
     {
         gameObject.SetActive(false);
