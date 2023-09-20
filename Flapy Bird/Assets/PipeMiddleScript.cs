6803757e97a77fb62d659b52c15c0611d13c4199
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-19 23:57:46 -0300

    .

--- Flapy Bird/Assets/PipeMiddleScript.cs
+++ Flapy Bird/Assets/PipeMiddleScript.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PipeMiddleScript : MonoBehaviour
 {
+
+    public LogicScript logic;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectsWithTag("Logic").GetComponent<LogicScript()>;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
