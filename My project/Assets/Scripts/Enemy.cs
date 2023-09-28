89f230eaebdb6dc13ef52790ee9910dae51bfabc
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-27 22:44:45 -0300

    ,

--- My project/Assets/Scripts/Enemy.cs
+++ My project/Assets/Scripts/Enemy.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10f;
+
+    private Transform target;
+
+    private int wavepointIndex = 0;
+
     void Start()
     {
-        
+        target = WayPoints.points[0];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.4f)
+        {
+            GetNextWayPoint();
+        }
+
+    }
+    void GetNextWayPoint()
+    {
+
+        if (wavepointIndex >= WayPoints.points.Length - 1 ) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = WayPoints.points[wavepointIndex];
     }
 }
