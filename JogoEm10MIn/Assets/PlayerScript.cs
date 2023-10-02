33571f0ee094ea68dd7a588c851906a956d4b8cd
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-10-02 20:00:26 -0300

    .

--- JogoEm10MIn/Assets/PlayerScript.cs
+++ JogoEm10MIn/Assets/PlayerScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 600f;
 
-    // Update is called once per frame
+    float movement = 0f;
+
+
     void Update()
     {
-        
+       movement = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
     }
 }
