fd03a03e78dac2a438deb38bfd27bef8c00a8f6d
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-10-25 22:57:44 -0300

    qw

--- Mini-Golf 2D/Assets/Scripts/Controle da bola de golf.cs
+++ Mini-Golf 2D/Assets/Scripts/Controle da bola de golf.cs
@@ -25,14 +25,35 @@ public class NewBehaviourScript : MonoBehaviour
 
    private void Start()
     {
-    
-     
+        oLineRenderer.positionCount = 2;
+        oLineRenderer.SetPosition(0,Vector2.zero);
+        oLineRenderer.SetPosition(1, Vector2.zero);
+        oLineRenderer.enabled = false;
+
+
     }
 
    
     private void Update()
     {
         pegarPosicaoDoMouse();
+
+        //GetMouseButtonDown Verifica se o mouse esta sendo apertado
+        if (Input.GetMouseButtonDown(0) && estaArrastando == false) 
+        {
+            IniciarArrastar();
+        }
+
+        if (estaArrastando)
+        {
+            Arrastar();
+        }
+
+        //GetMouseButtonUp Verifica se o mouse parou de ser apertado
+        if (Input.GetMouseButtonUp(0) && estaArrastando)
+        {
+            pararDeArrastar();
+        }
     }
 
     private void pegarPosicaoDoMouse()
@@ -40,6 +61,30 @@ public class NewBehaviourScript : MonoBehaviour
         posicaoDoMouse = cameraDoJogo.ScreenToWorldPoint(Input.mousePosition);
         posicaoDoMouse.z = 0f;
 
+    }
+
+    private void IniciarArrastar()
+    {
+        estaArrastando = true;
+
+        oLineRenderer.enabled = true;
+        oLineRenderer.SetPosition(0, posicaoDoMouse);
+        oLineRenderer.SetPosition(1,Vector2.zero);
+    }
+
+    private void Arrastar()
+    {
+        Vector3 posicaoInicial = oLineRenderer.GetPosition(0);
+        Vector3 posicaoAtual = posicaoDoMouse;
+
+        oLineRenderer.SetPosition(1, posicaoAtual);
+
+    }
+
+    private void pararDeArrastar()
+    {
+        estaArrastando = false;
+        oLineRenderer.enabled = false;
     }
 
 }
